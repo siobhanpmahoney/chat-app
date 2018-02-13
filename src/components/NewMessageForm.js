fcea6fd38011c43baa368f4ecc5ac384d2d3db06
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-02-12 21:42:28 -0500

    message form clearing upon submission

--- src/components/NewMessageForm.js
+++ src/components/NewMessageForm.js
@@ -11,8 +11,8 @@ class NewMessageForm extends React.Component {
 
   messageDraftListener = (event) => {
     event.preventDefault()
-    console.log(event.target.value)
       let draft = event.target.value
+
       this.setState({
         messageText: draft
       })
@@ -20,14 +20,16 @@ class NewMessageForm extends React.Component {
 
     messageSend = (event) => {
       event.preventDefault()
+
       let chat = this.props.chat
       let message = this.state.messageText
       this.props.handleNewMessageSubmit(event, chat, message)
+      event.target.parentElement.childNodes[0].value = ""
     }
 
 
   render(){
-    
+
     return(
       <div>
         <input type="text" onChange={this.messageDraftListener}/>
