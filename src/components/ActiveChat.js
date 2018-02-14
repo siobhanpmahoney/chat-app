0d051550c465aceb17b0fdcfb12acc0f475ee233
humzahchoudry <Humzah.choudry@gmail.com>
2018-02-14 11:57:27 -0500

    added usernames to chat

--- src/components/ActiveChat.js
+++ src/components/ActiveChat.js
@@ -37,13 +37,12 @@ class ActiveChat extends React.Component {
 
 
   render() {
-
         return (
           <div className="activeChatWindowContainer">
             <span className="topBar"><button onClick={this.closeChat}>close</button></span>
             <div className="messageHistory">
               {this.props.messages.map((m) => {
-                return <div key={m.id}><b>{this.props.user.username}</b>: {m.content}</div>
+                return <div key={m.id}><b>{m.username}</b>: {m.content}</div>
               })}
             </div>
             <div className="newMessageForm">
